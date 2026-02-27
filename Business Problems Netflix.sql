e035b573186964527e58f7a15fc79c821f53f1e1
Rajshree verma <141359943+28rajshree@users.noreply.github.com>
2026-02-27 21:20:21 +0530

    Create Business Problems Netflix.sql

---
+++ Business Problems Netflix.sql
@@ -0,0 +1,19 @@
+-- 18 Business Problems & Solutions
+1. Most Frequently Ordered Dishes by a Specific Customer in the Last Year
+2. Popular Time Slots for Orders (2-Hour Intervals)
+3. Average Order Value for High-Volume Customers
+4. High-Value Customers (Spending Over 100K)
+5. Orders Without Delivery
+6. Restaurant Revenue Ranking
+7. Most Popular Dish by City
+8. Cancellation Rate Comparison
+9. Rider Average Delivery Time
+10.Monthly Restaurant Growth Ratio
+11. Customer Segmentation
+12. Rider Monthly Earnings
+13.Order Frequency by Day
+14. Customer Lifetime Value (CLV)
+15.Monthly Sales Trends
+16. Rider Efficiency
+17. Order Item Popularity
+18.Monthly Restaurant Growth Ratio
